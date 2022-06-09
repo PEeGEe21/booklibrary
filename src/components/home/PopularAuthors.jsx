606299893ab9adfa9f31b02cae34b9b648580cd6
= <=>
2022-06-09 22:40:14 +0100

    made changes

--- src/components/home/PopularAuthors.jsx
+++ src/components/home/PopularAuthors.jsx
@@ -27,35 +27,40 @@ const PopularAuthors = () => {
                     <div className="mt-3 lg:mt-6 py-5 lg:flex-1 ">
                         <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
 
-                            {PopAuthors.map((author, index)=>(
-                                <div className="popular-authors relative rounded overflow-hidden  " key={author.id}>
-                                {/* <div> */}
-                                    {/* <span className="absolute right-0">Add to wishlist</span> */}
-                                {/* </div> */}
-                            {/* <button className="wish-button"><AiOutlineHeart className="w-full h-full"/></button> */}
-                                    <a href={`/author/${author.id}/${slugify(author.name)}`} className="pop_author-image-container">
-                                        <div className="pop_author">
-                                            <img src={`${author.image}`} alt="" className="w-full"/>
-                                        </div>
-                                    </a>
-                                    
-
-                                    <div className="px-2 py-2 mb-4 md:py-4 flex flex-col gap-2 text-center mt-0 lg:mt-2">
- 
-                                        <a href={`/author/${author.id}/${slugify(author.name)}`}>
-                                            <span className="book-title text-gray-900 text-lg mb-2">{author.name}</span>
+                        {PopAuthors.map((author, index)=>(
+                                    <div className="popular-authors relative rounded overflow-hidden shadow-md " key={author.id}>
+                                    {/* <div> */}
+                                        {/* <span className="absolute right-0">Add to wishlist</span> */}
+                                    {/* </div> */}
+                                {/* <button className="wish-button"><AiOutlineHeart className="w-full h-full"/></button> */}
+                                        <a href={`/author/${author.id}/${slugify(author.name)}`} className="">
+                                            <div className="relative overflow-hidden all-authors">
+                                                <img src={`${author.image}`} alt="" className="w-full"/>
+                                            </div>
                                         </a>
+                                        
 
-                                        <span className="text-gray-700 leading-none text-sm mb-2 ">{author.books_published} books published</span>
+                                        <div className="px-4 py-2 mb-4 md:py-4 flex flex-col gap-2 text-left mt-0 lg:mt-2">
+    
+                                            <a href={`/author/${author.id}/${slugify(author.name)}`}>
+                                                <span className="book-title text-gray-900 text-lg mb-2">{author.name}</span>
+                                            </a>
 
-                                        <div className="inline-flex items-center justify-center gap-4 flex-wrap">
-                                            <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#3b5999] hover:text-white transition delay-100 ease-in-out duration-300"><GrFacebookOption/></a>
-                                            <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#55acee] hover:text-white transition delay-100 ease-in-out duration-300"><GrTwitter/></a>
-                                            <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex  items-center justify-center bg-gray-200 hover:text-white hover:bg-[#0077B5] transition delay-100 ease-in-out duration-300"><GrLinkedinOption/></a>
+                                            <span className="text-gray-700 leading-none text-sm mb-2 ">{author.books_published} books published</span>
+
+                                            <div className="mt-3">
+                                                <a href={`/author/${author.id}/${slugify(author.name)}`} className="text-sm bg-blue-100/50 px-3 py-2 text-blue-500">view bio</a>
+                                            </div>
+
+                                            {/* <div className="inline-flex items-center justify-center gap-4 flex-wrap">
+                                                <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#3b5999] hover:text-white transition delay-100 ease-in-out duration-300"><GrFacebookOption/></a>
+                                                <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#55acee] hover:text-white transition delay-100 ease-in-out duration-300"><GrTwitter/></a>
+                                                <a href="" className="pop_author_social-icon h-8 w-8 rounded-full flex  items-center justify-center bg-gray-200 hover:text-white hover:bg-[#0077B5] transition delay-100 ease-in-out duration-300"><GrLinkedinOption/></a>
+                                            </div> */}
                                         </div>
-                                    </div>
-                            </div>
-                            ))}
+                                </div>
+                                ))}
+                            
                             
                         </div>
                     </div>
